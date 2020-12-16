e979c3c0680657d6a52296bfc723676d78ddd6da
Nate Sweet <nathanjsweet@pm.me>
2020-12-16 14:19:14 -0600

    bpf: Add Batch Methdods
    As of kernel v5.6 batch methods allow for the fast lookup,
    deletion, and updating of bpf maps so that the syscall
    overhead (repeatedly calling into any of these methods)
    can be avoided.
    
    The batch methods are as follows:
     * BatchUpdate
     * BatchLookup
     * BatchLookupAndDelete
     * BatchDelete
    
    Only the "array" and "hash" types currently support
    batch operations, and the "array" type does not support
    batch deletion.
    
    Tests are in place to test every scenario and helper
    functions have been written to catch errors that
    normally the kernel would give to helpful to users
    of the library.
    
    Signed-off-by: Nate Sweet <nathanjsweet@pm.me>

--- types.go
+++ types.go
@@ -100,6 +100,16 @@ func (mt MapType) canStoreProgram() bool {
 	return mt == ProgramArray
 }
 
+// canBatch returns true if the map type supports batch operations
+func (mt MapType) canBatch() bool {
+	return mt == Array || mt == Hash || mt == LRUHash || mt == PerCPUHash || mt == LRUCPUHash
+}
+
+// canBatchDelete returns true if the map type can batch delete
+func (mt MapType) canBatchDelete() bool {
+	return mt == Hash || mt == LRUHash || mt == PerCPUHash || mt == LRUCPUHash
+}
+
 // ProgramType of the eBPF program
 type ProgramType uint32
 
@@ -201,3 +211,13 @@ const (
 	// Pin an object by using its name as the filename.
 	PinByName
 )
+
+// BatchOptions batch map operations options
+//
+// Mirrors libbpf struct bpf_map_batch_opts
+// Currently BPF_F_FLAG is the only supported
+// flag (for ElemFlags).
+type BatchOptions struct {
+	ElemFlags uint64
+	Flags     uint64
+}
